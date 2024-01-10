ca0865c6a52f9ed1ebb7c9656ab032160fe0bef2
Romain GUIOT <romain.guiot@hotmail.fr>
2024-01-10 15:53:09 +0100

    correction de bug

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\RegistrationFormType;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
