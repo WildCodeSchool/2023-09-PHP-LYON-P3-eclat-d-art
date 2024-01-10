adff55937d2e1ab934c95c12a097e3034445803a
[Ryad] <[ryadzaatour@gmail.com]>
2024-01-10 15:39:43 +0100

    make back and front for user/artiste page. But is not finish

---
+++ src/Controller/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\ArtworkRepository;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user, ArtworkRepository $artworkRepository): Response
+    {
+        $artworks = $artworkRepository->findBy(['user' => $user]);
+
+
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+            'artworks' => $artworks,
+        ]);
+    }
+}
