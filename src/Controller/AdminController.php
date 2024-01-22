722886128dfa97eecb55867170f054973e68492f
Victor ANCLIN <victor.zano@gmail.com>
2024-01-22 17:20:42 +0100

    add a dashboard link and page, display all artist and artwork

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,12 @@ class AdminController extends AbstractController
 {
     #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'app_admin')]
-    public function adminDashboard(ArtworkRepository $artworkRepository): Response
+    public function adminDashboard(ArtworkRepository $artworkRepository, UserRepository $userRepository): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
             'artworks' => $artworkRepository->findAll(),
+            'users' => $userRepository->findAll(),
 
         ]);
     }
