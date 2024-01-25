45a2d18c9f04126716c6ead133017bc5a988d94c
Victor ANCLIN <victor.zano@gmail.com>
2024-01-25 15:24:53 +0100

    adding return to site button

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Controller\ArtworkController;
 use App\Entity\Artwork;
 use App\Entity\Category;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -36,5 +37,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Artwork', 'fas fa-list', Artwork::class);
         yield MenuItem::linkToCrud('Artist', 'fas fa-user-pen', User::class);
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToRoute('Retour sur le site', 'fa fa-circle-left', 'app_home');
     }
 }
