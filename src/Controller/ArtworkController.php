6c76dceac278e80e629c44e614572967a9ccbd57
Altyrion69 <altyrion@hotmail.fr>
2024-01-30 14:58:47 +0100

    correct appell

--- src/Controller/ArtworkController.php
+++ src/Controller/ArtworkController.php
@@ -21,8 +21,10 @@ class ArtworkController extends AbstractController
     #[Route('/', name: 'app_artwork_index', methods: ['GET'])]
     public function index(ArtworkRepository $artworkRepository): Response
     {
+            $artworkRepository->findAllByOrderDesc();
+
         return $this->render('artwork/index.html.twig', [
-            'artworks' => $artworkRepository->findAllByOrderDesc(),
+            'artworks' => $artworkRepository
         ]);
     }
 
